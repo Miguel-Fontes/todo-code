de2e502b178d34fe1ea41a89335a680e9a689b91
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-01-23 10:39:04 -0200

    Limpeza de código

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-/*'use strict'
-var directory = require('./directory')({path: './/..//', recursive: true, extensions: ['.js'], ignore: ['node_modules', '.git', 'vscode', 'dist']}) // Para um teste mais legal -> .//..//..//loja-concept//
-var file = require('./file')({parse: [require('./todo-parse'), require('./slash-parse')]})
-var container = require('./container')
-
-try {
-  let archive = container({value: directory.getContent()})
-
-  archive.map(function (f) {
-    return file
-      .open(f)
-      .parse()
-      .getData()
-  })
-
-  archive.filter((f) => {
-    if (f.content.length > 0) {
-      return true
-    }
-  })
-
-  archive.log()
-
-} catch (e) {
-  console.log(e)
-}*/
-
 const electron = require('electron')
 const app = electron.app; // Module to control application life.
 const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
