a1e80b27adb298410c83fa1e4fc17f4e79b343c5
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-01-28 11:34:08 -0200

    Implementada lógica para carga de Todos de projetos selecionados em lista de projetos.

--- src/run.js
+++ src/run.js
@@ -1,46 +1,77 @@
 'use strict'
-var directory = require('./../directory')({path: './/..//Highland//', recursive: true, extensions: ['.js'], ignore: ['node_modules', '.git', 'vscode', 'dist']})
-var file = require('./../file')({parse: [require('./../todo-parse')]})
-var container = require('./../container')
+let run = (function (e) {
+  /*let directory = require('./../directory')({path: './/..//Highland//', recursive: true, extensions: ['.js'], ignore: ['node_modules', '.git', 'vscode', 'dist']}),
+  file = require('./../file')({parse: [require('./../todo-parse')]}),
+  container = require('./../container'),
+*/
+  let directory = require('./../directory'),
+    file = require('./../file')({parse: [require('./../todo-parse')]}),
+    container = require('./../container'),
+    path = e.target.attributes['x-data-path'].value,
+    projectFiles
 
-// TODO: Extrair a library de tratamento de arquivos para módulo separado e publicar no NPM. Usar aqui como dependência.
-// TODO: Pesquisar formas de templating para implementar cards
-// TODO: Possibilitar configuração da aplicação (seleção de diretório para ser monitorado)
-// TODO: Empacotar aplicação
-// TODO: Tratamento de parâmetros e diretórios
+  // TODO: Extrair a library de tratamento de arquivos para módulo separado e publicar no NPM. Usar aqui como dependência.
+    // TODO: Pesquisar formas de templating para implementar cards
+    // TODO: Possibilitar configuração da aplicação (seleção de diretório para ser monitorado)
+    // TODO: Empacotar aplicação
+    // TODO: Tratamento de parâmetros e diretórios
 
-try {
-  let archive = container({value: directory.getContent()})
-
-  archive.map(function (f) {
-    return file
-      .open(f)
-      .parse()
-      .getData()
+  projectFiles = directory({
+    path: path,
+    recursive: true,
+    extensions: ['.js'],
+    ignore: ['node_modules', '.git', 'vscode', 'dist']
   })
 
-  archive.filter((f) => {
-    if (f.content.length > 0) {
-      return true
-    }
-  })
+  try {
+    let archive = container({value: projectFiles.getContent()})
 
-  const todos = archive.get()
-  document.write('<div class="c-todo-container">')
-  document.write('<div class="c-todo-panel">')
-  todos.forEach(function (value) {
-    document.write('<section class="c-todo-card t-box-shadow">')
-    document.write('<p>' + value.name + ' - [' + value.path + ']' + '<p>')
-    document.write('<ul class="c-todo-list o-list-unstyled">')
-    value.content.forEach(function (content) {
-      document.write('<li><i class="fa fa-angle-double-right"></i>' + content + '</li>')
+    archive.map(function (f) {
+      return file
+        .open(f)
+        .parse()
+        .getData()
     })
-    document.write('</ul>')
-    document.write('</section>')
-  })
-  document.write('</div>')
-  document.write('</div>')
 
-} catch (e) {
-  console.log(e)
-}
+    archive.filter((f) => {
+      if (f.content.length > 0) {
+        return true
+      }
+    })
+
+    const todos = archive.get()
+
+    let todoPanel = document.getElementsByClassName('c-todo-panel')[0]
+    todoPanel.innerHTML = ''
+    let card, title, list, todo, todoMarker
+
+    todos.forEach(function (value) {
+      card = document.createElement('section')
+      card.classList.add('c-todo-card')
+      card.classList.add('t-box-shadow')
+
+      title = document.createElement('p')
+      title.innerHTML = (value.name + ' - [' + value.path + ']')
+
+      list = document.createElement('ul')
+      list.classList.add('c-todo-list')
+      list.classList.add('o-list-unstyled')
+
+      value.content.forEach(function (content) {
+        todo = document.createElement('li')
+        todoMarker = document.createElement('i')
+        todoMarker.classList.add('fa')
+        todoMarker.classList.add('fa-angle-double-right')
+        todo.appendChild(todoMarker)
+        todo.innerHTML += content
+        list.appendChild(todo)
+      })
+
+      card.appendChild(title)
+      card.appendChild(list)
+      todoPanel.appendChild(card)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+})
